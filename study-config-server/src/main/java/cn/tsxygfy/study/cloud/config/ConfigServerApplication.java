35b69c1d327866267ad3c7b4e5a33df29a67f9aa
guofy4 <guofy4@lenovo.com>
2020-09-22 14:56:16 +0800

    config

--- study-config-server/src/main/java/cn/tsxygfy/study/cloud/config/ConfigServerApplication.java
+++ study-config-server/src/main/java/cn/tsxygfy/study/cloud/config/ConfigServerApplication.java
@@ -1,5 +1,6 @@
 package cn.tsxygfy.study.cloud.config;
 
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
@@ -12,4 +13,7 @@ import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 @EnableEurekaClient
 @EnableConfigServer
 public class ConfigServerApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigServerApplication.class, args);
+    }
 }
